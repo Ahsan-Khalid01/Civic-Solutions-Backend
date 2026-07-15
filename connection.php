dd377a8db861bed13a756e0646ac762f6f5ee9d8
Ahsan Khalid <ahsankhalid0968@gmail.com>
2026-07-15 23:15:34 +0500

    today changes

---
+++ connection.php
@@ -0,0 +1,15 @@
+<?php
+ $servername = "localhost";
+  $username = "root"; 
+  $password = "";
+   $dbname = "civicsolution";
+    $conn=mysqli_connect($servername,$username,$password,$dbname); 
+    // if(!$conn) 
+    //     {
+    //      echo "Connection failed: " ; 
+    //      }
+    //  else {
+    //      echo "Connected successfully"; 
+    //  }
+    // 
+    //  ?>
